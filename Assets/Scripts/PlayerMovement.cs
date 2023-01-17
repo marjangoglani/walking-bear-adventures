e0e976292ed4ea013527152c1f75b20aec5206b2
Marjan Goglani <marjangoglani@Marjans-MacBook-Pro.local>
2023-01-16 19:48:14 -0600

    ugly movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Transform player;
-    private float speed = 5f;
+    private float speed = 1f;
 
     // Update is called once per frame
     void Update()
     {
         float horizontalDirection = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        player.position = player.position + new Vector3(horizontalDirection, 0);
+        float verticalDirection = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        player.position = player.position + new Vector3(horizontalDirection, verticalDirection);
     }
 }
