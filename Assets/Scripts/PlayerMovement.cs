a9936a0bb9a7addb5e1aae395032791ae1089c10
Marjan Goglani <marjangoglani@infra04-wg012.eduroam.wireless.umn.edu>
2023-01-18 11:17:14 -0600

    animation is weird, animation gets stuck on left walking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,56 @@ public class PlayerMovement : MonoBehaviour
 {
     public Transform player;
     private float speed = 1f;
+    public Animator animator;
+    private bool IsFacingRight = true;
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalDirection = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        float verticalDirection = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        player.position = player.position + new Vector3(horizontalDirection, verticalDirection);
+        float horizontalDirection = Input.GetAxisRaw("Horizontal");
+        float verticalDirection = Input.GetAxisRaw("Vertical");
+
+        float horizontalSpeed = horizontalDirection * speed * Time.deltaTime;
+        float verticalSpeed = verticalDirection * speed * Time.deltaTime;
+
+
+        // FIXME: if verticalDirection is negative, ge tthe transform of the player and flip it around the y axis
+        if (horizontalDirection < 0)
+        {
+            // FIXME: I will probably have to either change the animation manually (probably not possible)
+            // or I can use the variables that I defined for the checking of animator
+
+            // set horizontal walky animation
+            animator.SetInteger("HorSpeed", 1);
+            player.localScale = new Vector3(-1, player.localScale.y);       // flip
+            IsFacingRight = false;
+
+        }
+        else if (horizontalDirection > 0)
+        {
+            // set horizontal walky animation
+            animator.SetInteger("HorSpeed", 1);
+            if (!IsFacingRight)
+            {
+                player.localScale = new Vector3(-1, player.localScale.y);
+                IsFacingRight = true;
+            }
+        }
+        else
+        {
+            // animation idle
+            animator.SetInteger("HorSpeed", 0);
+            animator.SetInteger("VerSpeed", 0);
+        }
+
+        player.position = player.position + new Vector3(horizontalSpeed, verticalSpeed);
     }
 }
+
+
+
+/*
+ * Today:
+ * 2. implement movement script
+ * 3. fix animations on the bear (left and right walking)
+ */
